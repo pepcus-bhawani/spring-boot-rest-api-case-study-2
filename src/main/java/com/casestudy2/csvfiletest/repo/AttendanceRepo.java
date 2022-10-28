a1f7e975722679af800eb9690f0f22135f636dc7
pepcus-bhawani <111972697+pepcus-bhawani@users.noreply.github.com>
2022-10-28 19:21:41 +0530

    Update AttendanceRepo.java

--- src/main/java/com/casestudy2/csvfiletest/repo/AttendanceRepo.java
+++ src/main/java/com/casestudy2/csvfiletest/repo/AttendanceRepo.java
@@ -1,5 +1,4 @@
 package com.casestudy2.csvfiletest.repo;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
